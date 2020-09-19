75199d7758a129ee388cf5899881b0486756f64f
andleo17 <andres.gastulo@gmail.com>
2020-09-19 09:27:43 -0500

    Realizando el menu

---
+++ app/src/main/java/com/andres/notasusat/ui/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.andres.notasusat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.andres.notasusat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener((View v) -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
+    }
+}
