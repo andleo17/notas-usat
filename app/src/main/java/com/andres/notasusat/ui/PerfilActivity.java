c8ebd4b081837d54884fe14209b27a74d89914b1
PaolaCieza <paolacieza8@gmail.com>
2020-10-27 02:06:58 -0500

    diseño perfil

---
+++ app/src/main/java/com/andres/notasusat/ui/PerfilActivity.java
@@ -0,0 +1,17 @@
+package com.andres.notasusat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.andres.notasusat.R;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
