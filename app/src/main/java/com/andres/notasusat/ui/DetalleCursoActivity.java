e3995bc9be271871316530738d15f2ad4728dcbd
PaolaCieza <paolacieza8@gmail.com>
2020-10-30 22:38:06 -0500

    avanzando detalle curso

---
+++ app/src/main/java/com/andres/notasusat/ui/DetalleCursoActivity.java
@@ -0,0 +1,16 @@
+package com.andres.notasusat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.andres.notasusat.R;
+
+public class DetalleCursoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_curso);
+    }
+}
