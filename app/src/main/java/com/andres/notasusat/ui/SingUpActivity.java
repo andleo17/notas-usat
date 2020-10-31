b844ea6e0ae681c27366204108be06c0e0a39a71
PaolaCieza <paolacieza8@gmail.com>
2020-10-31 01:26:03 -0500

    detallecurso

--- app/src/main/java/com/andres/notasusat/ui/SingUpActivity.java
+++ app/src/main/java/com/andres/notasusat/ui/SingUpActivity.java
@@ -121,7 +121,7 @@ public class SingUpActivity extends AppCompatActivity  {
     }
 
     private void setGenre(){
-        Spinner spGenre = (Spinner)findViewById(R.id.spinnerGenrePerfil);
+        Spinner spGenre = (Spinner)findViewById(R.id.spinnerGenero);
 
         spGenre.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
