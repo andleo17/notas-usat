a27bce99b357de17023f248a0c3f8ca79ca62dda
jesuslujan98 <51263684+jesuslujan98@users.noreply.github.com>
2020-11-04 07:36:32 -0500

    conexion con token

---
+++ app/src/main/java/com/andres/notasusat/ui/Apollo.kt
@@ -0,0 +1,42 @@
+package com.andres.notasusat.ui
+
+import android.content.Context
+import android.os.Looper
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+
+private var instance: ApolloClient? = null
+
+fun apolloClient(context: Context): ApolloClient {
+    check(Looper.myLooper() == Looper.getMainLooper()) {
+        "Only the main thread can get the apolloClient instance"
+    }
+
+    if (instance != null) {
+        return instance!!
+    }
+
+    val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(AuthorizationInterceptor(context))
+            .build()
+
+    instance = ApolloClient.builder()
+            .serverUrl("https://notas-gql.herokuapp.com/graphql/endpoint")
+            .okHttpClient(okHttpClient)
+            .build()
+
+    return instance!!
+}
+
+private class AuthorizationInterceptor(val context: Context) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiVVNFUiJ9.V4ZQ7c89g1z39DB_U7BzrXzFsQbKtlXPFLUXqw1unfk")
+                .build()
+
+        return chain.proceed(request)
+    }
+}
