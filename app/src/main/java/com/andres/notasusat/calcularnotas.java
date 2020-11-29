d563517f939df0767a75ad58406b737b4e8c4136
PaolaCieza <paolacieza8@gmail.com>
2020-11-29 02:37:35 -0500

    xdd

---
+++ app/src/main/java/com/andres/notasusat/calcularnotas.java
@@ -0,0 +1,14 @@
+package com.andres.notasusat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calcularnotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcularnotas);
+    }
+}
